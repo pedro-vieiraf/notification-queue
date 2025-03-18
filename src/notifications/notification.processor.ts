9962aa4b8ef1ab8ecb4a3066ce3f69b609bcd933
Pedro Vieira <pedrovieirafdev@gmail.com>
2025-03-17 22:00:52 -0300

    feat: Finished queue function to send real emails

--- src/notifications/notification.processor.ts
+++ src/notifications/notification.processor.ts
@@ -1,16 +1,39 @@
 import { OnWorkerEvent, Processor, WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
 import { NotificationDto } from './dto/notification.dto';
+import { Inject } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Processor('notification', { concurrency: 5 })
 export class NotificationProcessor extends WorkerHost {
+  constructor(
+    @Inject(MailerService) private readonly mailerService: MailerService,
+  ) {
+    super();
+  }
   async process(job: Job) {
     const { type, recipient, message } = job.data as NotificationDto;
 
     switch (type) {
       case 'email':
         console.log(`Sending EMAIL to ${recipient}: ${message}`);
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+
+        // Envio de e-mail usando o MailerService
+        try {
+          await this.mailerService.sendMail({
+            to: recipient, // Destinatário
+            subject: 'Notification', // Assunto
+            text: message, // Corpo do email
+            html: `
+              <h1>Notificação Importante</h1>
+              <p><strong>Olá, ${recipient}!</strong></p>
+              <p>${message}.</p>`, // Corpo do email em HTML
+          });
+          console.log(`Email sent to ${recipient}`);
+        } catch (error) {
+          console.error(`Failed to send email to ${recipient}:`, error);
+        }
+
         break;
 
       case 'sms':
--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -3,14 +3,36 @@ import { NotificationController } from './notifications.controller';
 import { BullModule } from '@nestjs/bullmq';
 import { NotificationProcessor } from './notification.processor';
 import { NotificationEventsListener } from './notification.events';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { NotificationService } from './notifications.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     BullModule.registerQueue({
       name: 'notification',
     }),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get<string>('EMAIL_HOST'),
+          port: configService.get<number>('EMAIL_PORT'),
+          auth: {
+            user: configService.get<string>('EMAIL_USER'),
+            pass: configService.get<string>('EMAIL_PASS'),
+          },
+        },
+      }),
+    }),
   ],
   controllers: [NotificationController],
-  providers: [NotificationProcessor, NotificationEventsListener],
+  providers: [
+    NotificationProcessor,
+    NotificationEventsListener,
+    NotificationService,
+  ],
 })
 export class NotificationsModule {}
